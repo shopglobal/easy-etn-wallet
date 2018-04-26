09738427e7b8ca417cd69fe05bdd4309a8ce3cea
Interchained <president@worldvaporexpo.com>
2018-04-26 17:57:09 -0400

    Update daemon.service.ts

--- src/app/core/daemon.service.ts
+++ src/app/core/daemon.service.ts
@@ -6,7 +6,7 @@ import { makeUrl, Daemon } from './monerod/monerod';
 export class DaemonService {
 
   // Daemon Connect
-  url = makeUrl('http', '66.175.216.72', '26968', 'getheight');
+  url = makeUrl('http', '127.0.0.1', '48091', 'getheight');
   mydaemon = Daemon(this.url);
 
   getDaemonHeight() {
